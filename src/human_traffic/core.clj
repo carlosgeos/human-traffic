7a9f6a2b6cf8ff4ac27a14073c416d286cc62d1f
Carlos Requena López <crequena@ulb.ac.be>
2019-01-26 17:01:26 +0100

    adapt response to AWS Lambda proxy

--- src/human_traffic/core.clj
+++ src/human_traffic/core.clj
@@ -1,7 +1,29 @@
 (ns human-traffic.core
   (:gen-class
-   :methods [^:static [handler [String] String]]))
+   :methods [^:static [handler [Object] Object]])
+  (:require [cheshire.core :as che]))
 
 
-(defn -handler [s]
-  "Response")
+;;; Because this is on AWS Lambda + API Gateway, there is no need for
+;;; any HTTP servers or routing middleware. Just fetch from DB and
+;;; return JSON
+
+;;; AWS Lambda proxy response format
+;;; {
+;;;    "isBase64Encoded": true/false,
+;;;    "statusCode": 200,
+;;;    "headers": {},
+;;;    "body": "The body" ;; <-- stringified body !
+;;; }
+
+(def lambda_default
+  {"isBase64Encoded" false
+   "headers" {}
+   "statusCode" 200})
+
+
+(defn -handler
+  "API Gateway transforms query GET params into POST fields, which can
+  be found in s"
+  [s]
+  (merge lambda_default {"body" (che/generate-string s)}))
